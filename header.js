982a4ca29bb0a203fef1af90d09516b6e1b49dec
osaid <redfirerpk@gmail.com>
2024-01-19 00:47:32 +0300

    Added dropdown functionality and fetching for countries

--- header.js
+++ header.js
@@ -6,10 +6,17 @@ function saveToStorage(theme) {
   localStorage.setItem("theme", theme);
 }
 
+function checkIfLight() {
+  return localStorage.getItem("theme") === "light-theme";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const img = document.createElement("img");
   img.classList.add("theme-icon");
-  img.src = document.body.classList.contains("light-theme") ? moonUrl : sunUrl;
+  img.src = checkIfLight() ? moonUrl : sunUrl;
+  checkIfLight()
+    ? document.body.classList.add("light-theme")
+    : document.body.classList.remove("light-theme");
   img.alt = "change theme";
   img.role = "button";
   img.tabIndex = "0";
@@ -17,16 +24,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const inner = `<h1 class="header-title">Where in the world?</h1>
       <div class="theme-selector">
         ${img.outerHTML}
-        <p>Dark mode</p>
+        <p>${checkIfLight() ? "Light mode" : "Dark mode"}</p>
       </div>`;
 
   document.body.querySelector("header").innerHTML = inner;
-  const selectedImage = document.querySelector(".theme-icon");
+  const themeSelector = document.querySelector(".theme-selector");
+  const selectedImage = themeSelector.querySelector(".theme-icon");
 
   selectedImage.addEventListener("click", () => {
-    document.body.classList.toggle("light-theme");
+    const isLight = document.body.classList.toggle("light-theme");
+    const theme = isLight ? "light-theme" : "";
+    themeSelector.querySelector("p").textContent = isLight
+      ? "Light mode"
+      : "Dark mode";
     selectedImage.src = document.body.classList.contains("light-theme")
       ? moonUrl
       : sunUrl;
+
+    saveToStorage(theme);
   });
 });
---
+++ index.js
@@ -0,0 +1,63 @@
+const countryList = document.querySelector(".country-list");
+const dropdownButton = document.querySelector("#dropdownMenuButton");
+const optionsContainer = document.querySelector("#optionsList");
+const options = optionsContainer.querySelectorAll("a");
+
+async function fetchData() {
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  console.log(data[0]);
+  populate(data);
+}
+
+fetchData();
+
+function expandOptions() {
+  optionsContainer.classList.toggle("active");
+  if (optionsContainer.classList.contains("active")) {
+    options.forEach((option) => (option.tabIndex = "0"));
+    this.ariaExpanded = true;
+  } else {
+    options.forEach((option) => (option.tabIndex = "-1"));
+    this.ariaExpanded = false;
+  }
+}
+
+// document.addEventListener("DOMContentLoaded", async () => {});
+
+function populate(countries) {
+  countries.slice(0, 3).map((country) => {
+    const countryCard = document.createElement("article");
+    // <article class="country-card">
+    countryCard.classList.add("country-card");
+    countryCard.innerHTML = `
+          <a class="image-container" href="/pages/details.html?country=${
+            country.name
+          }">
+            <img src="${country.flags.png}" alt="country flag" />
+          </a>
+          <div class="country-card-info">
+            <a href="/pages/details.html?country=${country.name}">
+              <h2 class="country-card-title">${country.name}</h2>
+            </a>
+            <ul>
+              <li>
+                <strong>Population:</strong>
+                <span>${country.population.toLocaleString()}</span>
+              </li>
+              <li>
+                <strong>Region:</strong>
+                <span>${country.region}</span>
+              </li>
+              <li>
+                <strong>Capital:</strong>
+                <span>${country.capital}</span>
+              </li>
+            </ul>
+          </div>`;
+
+    countryList.appendChild(countryCard);
+  });
+}
+
+dropdownButton.addEventListener("click", expandOptions);
