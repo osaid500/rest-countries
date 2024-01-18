461457b332bf84a69138e83f77f9c89281fb089d
osaid <redfirerpk@gmail.com>
2024-01-18 18:02:41 +0300

    added theme toggle functionality

--- header.js
+++ header.js
@@ -1,16 +1,32 @@
 const isRoot = window.location.pathname.endsWith("index.html");
-const url = isRoot ? "./icons/sun.svg" : "../icons/sun.svg";
-const inner = `<h1 class="header-title">Where in the world?</h1>
+const sunUrl = isRoot ? "./icons/sun.svg" : "../icons/sun.svg";
+const moonUrl = isRoot ? "./icons/moon.svg" : "../icons/moon.svg";
+
+function saveToStorage(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.createElement("img");
+  img.classList.add("theme-icon");
+  img.src = document.body.classList.contains("light-theme") ? moonUrl : sunUrl;
+  img.alt = "change theme";
+  img.role = "button";
+  img.tabIndex = "0";
+
+  const inner = `<h1 class="header-title">Where in the world?</h1>
       <div class="theme-selector">
-        <img
-          src=${url}
-          alt="change theme"
-          role="button"
-          tabindex="0"
-        />
+        ${img.outerHTML}
         <p>Dark mode</p>
       </div>`;
 
-document.addEventListener("DOMContentLoaded", () => {
   document.body.querySelector("header").innerHTML = inner;
+  const selectedImage = document.querySelector(".theme-icon");
+
+  selectedImage.addEventListener("click", () => {
+    document.body.classList.toggle("light-theme");
+    selectedImage.src = document.body.classList.contains("light-theme")
+      ? moonUrl
+      : sunUrl;
+  });
 });
